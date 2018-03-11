b6b071597be4510a2d735e0506c3326779d66be8
Gansheng <gansheng.tan@student.ecp.fr>
2018-03-11 22:21:22 +0100

    deleting unrelevent code

--- agenda/index.php
+++ agenda/index.php
@@ -4,10 +4,7 @@
 <head>
 	<meta charset="UTF-8">
     <title>Agenda</title>
-	<title>Document</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Agenda</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to9aafd9a7d8d0e6285f7579
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="../_css/style.css">
 	<link rel="stylesheet" href="../_css/connexion.css">
