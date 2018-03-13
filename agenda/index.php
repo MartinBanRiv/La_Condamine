76e6f99da07cf95cb859ea8d1c459789bef6ba7c
Gansheng <gansheng.tan@student.ecp.fr>
2018-03-13 19:53:24 +0100

    rapport avec css

--- agenda/index.php
+++ agenda/index.php
@@ -1,9 +1,8 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
     <title>Agenda</title>
+	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="../_css/style.css">
@@ -89,107 +88,24 @@
 			  start: '2018-02-28'
 			}
 			]
-    	});
-</head>
-    
+    	})});
+    </script>
+                                           
 
-                                             
-</html>
-                                             
-	<link rel="stylesheet" href="../_css/agenda.css">
-	<link href='fullcalendar/fullcalendar.min.css' rel='stylesheet'>
-	<link href='fullcalendar/fullcalendar.print.min.css' rel='stylesheet' media='print'>
-	<link rel="stylesheet" href="niceDatePicker-master/nice-date-picker.css">
-
-	<script src='fullcalendar/lib/moment.min.js'></script>
-	<script src='fullcalendar/lib/jquery.min.js'></script>
-	<script src='fullcalendar/fullcalendar.min.js'></script>
-	<script src='fullcalendar/locale/fr.js'></script>
-	<script src="niceDatePicker-master/nice-date-picker.js"></script>
-	<script>
-	$(document).ready(function() {
-	$('#calendar').fullCalendar({
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay,listMonth'
-			},
-			themeSystem: 'bootstrap3',
-			defaultDate: '2018-02-12',
-			weekNumbers: true,
-			navLinks: true, // can click day/week names to navigate views
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-			events: [
-			{
-			  title: 'All Day Event',
-			  start: '2018-02-01'
-			},
-			{
-			  title: 'Long Event',
-			  start: '2018-02-07',
-			  end: '2018-02-10'
-			},
-			{
-			  id: 99,
-			  title: 'Repeating Event',
-			  start: '2018-02-09T16:00:00'
-			},
-			{
-			  id: 99,
-			  title: 'Repeating Event',
-			  start: '2018-02-16T16:00:00'
-			},
-			{
-			  title: 'Conference',
-			  start: '2018-02-11',
-			  end: '2018-02-13'
-			},
-			{
-			  title: 'Meeting',
-			  start: '2018-02-12T10:30:00',
-			  end: '2018-02-12T12:30:00'
-			},
-			{
-			  title: 'Lunch',
-			  start: '2018-02-12T12:00:00'
-			},
-			{
-			  title: 'Meeting',
-			  start: '2018-02-12T14:30:00'
-			},
-			{
-			  title: 'Happy Hour',
-			  start: '2018-02-12T17:30:00'
-			},
-			{
-			  title: 'Dinner',
-			  start: '2018-02-12T20:00:00'
-			},
-			{
-			  title: 'Birthday Party',
-			  start: '2018-02-13T07:00:00'
-			},
-			{
-			  title: 'Click for Google',
-			  url: 'http://google.com/',
-			  start: '2018-02-28'
-			}
-			]
-    	});
-	});
-	</script>
-	<style>
+<style>
 	#calendar {
 		max-width: 1000px;
 		margin: 0 auto;
 	}
-	</style>
+</style>
+    
 </head>
                                              
                                              
 <body>
 	<?php include '../_views/header.html' ?>
+    
+    
 
 	<div id="main" class="container-fluid">
 		<div class="col-lg-8">
@@ -228,11 +144,17 @@
 </body>
 </html>
 
-// balise correspondu
-// header footer fonction pas
-
-// en general
-//id=main class=container-fluid main in the agenda.css  id=main
-// inline class calendar  max-width et margin
-//class fc fc-bootstrap3 fc-ltr
-
+<script>
+*/ balise correspondu/*
+*/header footer fonction pas/*
+*/en general/*
+*/id=main class=container-fluid main in the agenda.css  id=main/*
+*/ inline class calendar  max-width et margin/*
+*/class fc fc-bootstrap3 fc-ltr est tout l'agenda  qui est en relation qvec fullcalendar.min.css  .fc /*
+*/ class= fc-toolbar fc-header-toolbar est le onglet au dessus   qui est aussi en relation avec fullcalendar.min.css  .fc toolbar/*
+*/ fc left est aujourd'hui+btn-group mais le btn ne s'affiche pas  .btn-group est dqns bootstrap.min.css  .btn-group/*
+*/ pour les duex botton, btn est dans bootstrap3, .fc bottin est dans fullcalendar.min.css /*
+*/fc-right,center in fullcalendar.min.css /*
+*/ table-bordered est dans fullcalebdar       class=fc-head-container sont sem lun mar mer  est dans .fc td de full.min.css/*
+*/ need to understand the document ready fonction /*
+</script>
