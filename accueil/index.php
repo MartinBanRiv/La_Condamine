f1f4839dd3af4780a945aa51002cc4027e47faca
Martin BanRiv <martin.banet-rivet@student.ecp.fr>
2018-04-04 10:56:48 +0100

    ajout page d'accueil

---
+++ accueil/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="../_css/style.css">
+	<link rel="stylesheet" href="../_css/accueil.css">
+	<link rel="stylesheet" href="../_css/signin.css">
+</head>
+<body>
+	<?php include '../_views/header.html' ?>
+
+	<div id="main" class="container-fluid">
+		<div class="row" id="bigrow">
+
+			<div id="content" class="col-lg-9 pull-lg-3 align-items-center">
+
+			<div class="row" id="firstrow">
+
+				<div class="image-large">
+							<img src="../_img/banner1_large.jpg" class="w-100">
+						</div>
+
+				<div id="body" class="col-lg-12">
+					<div class="article">
+												
+							<div class="container-fluid">
+
+								<h3>BIENVENUE DANS LA COMMUNAUTE ARTISTIQUE	DE LA CONDAMINE</h3> 
+								<p>
+									La Condamine est une communauté d'artistes entrepreneurs. Ensemble, nous développons des solutions innovantes pour que chacun vive de son art.
+									Parce qu'aujourd'hui ce qui fait le succès d'un artiste, c'est sa capacité à communiquer sur sa création, à la mettre en valeur et à saisir les opportunités qui s'offrent à lui, nous proposons à nos membres un suivi personnalisé en vue de structurer leur projet de création aux niveaux commercial, stratégique et administratif. Donner à nos artistes toutes les outils pour percer, c'est leur permettre de courtcircuiter les intermédiaires traditionnels et de parvenir à l'autonomie créative. </br> Grâce à une formation continue, nous rendons les méthodes utilisées par les entreprises accessibles à nos membres. Nous mettons en relation nos artistes pour qu’ils profitent de leurs complémentarités et leur ouvrons les portes des acteurs du monde de la culture pour qu'ils développent un modèle économique viable autour de leurs pratiques. </br> Notre équipe grandit chaque jour pour mutualiser les coûts liés à la diffusion artistique, aider chaque artiste à articuler sa vision de façon claire et donner à son art la visibilité qu'il mérite.
+								</p>
+							</div>
+						</div>	
+			
+				</div>
+				
+			</div>
+
+
+
+			<div class="row" id="secondrow">	
+				<div id="forum" class="col-lg-7 pull-lg-5">	
+					<p> FORUM </p>
+				</div>
+
+				<div id="agenda" class="col-lg-5 push-lg-7">
+					<p> AGENDA </p>
+				</div>
+
+
+			</div>
+
+		</div>
+
+			<div id="colart" class="col-lg-3 push-lg-9 align-items-center">
+				<div class="container-fluid" id="artdm">
+					<p> Artiste du moment à trouver </p>
+				</div>	
+			</div>	
+
+		</div>
+
+		<div class="row" id="secondrow">
+
+			
+			<?php include '../_views/footer.html' ?>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
